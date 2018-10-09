69e6075703820cc6087738df78b3ce34e880e112
Vinicius Oliveira <vinicius.olliveira@outlook.com>
2018-10-09 02:02:18 -0300

    Organização do código

--- serverstarter.py
+++ serverstarter.py
@@ -7,12 +7,6 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-
-    :return:        the rendered template 'home.html'
-    """
     return render_template('home.html')
 
 if __name__ == '__main__':
